5317b0daa45e88fbdada9a29797c77f6767ab6bd
Verolian <verolianopiyo@gmail.com>
2024-03-22 16:17:33 +0300

    Fisrt Add

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
---SQL script to insert the new row into the first_table.
+-- SQL script to insert the new row into the first_table.
 INSERT INTO first_table (id, name)
 VALUES (89, 'Holberton School');
