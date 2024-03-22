5a3e6bcdbb67125c34d6de71b52fb2f36aa8c8df
Verolian <verolianopiyo@gmail.com>
2024-03-22 16:19:20 +0300

    Count 89

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
---SQL script to display the number of records with id = 89 in the first_table.
+-- SQL script to display the number of records with id = 89 in the first_table.
 SELECT COUNT(*) AS number-of-records
 FROM first_table
 WHERE id = 89;
