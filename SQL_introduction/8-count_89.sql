1e6a382347e79b9cc741be65174e38649dd6b900
Verolian <verolianopiyo@gmail.com>
2024-03-22 16:26:47 +0300

    Count 89

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
 -- SQL script to display the number of records with id = 89 in the first_table.
-SELECT COUNT(*) AS number-of-records
+SELECT COUNT(*) AS number_of_records  -- Corrected alias name
 FROM first_table
 WHERE id = 89;
