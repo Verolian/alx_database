a83f49739f890e09ced06f917ee30dcf1830a802
Verolian <verolianopiyo@gmail.com>
2024-03-22 16:02:45 +0300

    First Table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
---SQL script to create the table first_table with the specified columns.
+-- SQL script to create the table first_table with the specified columns.
 CREATE TABLES IF NOT EXIST first_table (
     id INT PRIMARY KEY,
     name VARCHAR(256)
